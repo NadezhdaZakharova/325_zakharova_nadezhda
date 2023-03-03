e0b56ed8a855df8722a1f29c16dabf75f7b2e5f8
NadezhdaZakharova <nadika.zakharova@gmail.com>
2023-03-03 22:41:55 +0300

    update 2

--- 25.02.py
+++ 25.02.py
@@ -1,20 +1,17 @@
 import math
 print ('Начало расчета')
-G0 = 6.674*math.pow(10,-11)
-M1 = 5.97600*math.pow(10,24)
+def power(M2, r):
+    G0 = 6.6743*(math.pow(10,-11))
+    M1 = 5.97600 * math.pow(10,24)
+    return (G0*M1*M2)/(math.pow(r,2))
 print('Введите расcтояние между объектами')
 S = float(input())
 print ('Введите массу выбранного объекта')
-M2 = float(input())
+M = float(input())
 print ('Введите cтепень 10 массы второго объекта')
 mst=float(input())
-M2=M2*math.pow(10,mst)
-print ('Введите cтепень 10 расстояния до второго объекта')
-rst=float(input())
-S=S*math.pow(10,rst)
-F0=(G0*M1*M2)
-F00=(math.pow(S,2))
-F=F0/F00
+M2= M*math.pow(10,mst)
+F = power(M2,S)
 print (F)
 
 

aa70d58f9bc2a861b7c02408aa19da88ba1c9c53
NadezhdaZakharova <nadika.zakharova@gmail.com>
2023-03-25 01:52:59 +0300

    /

---
+++ Task2.py
@@ -0,0 +1,11 @@
+import random
+List_1 = []
+List_2 = []
+for i in range(50,101):
+    List_1.append(random.randint(0,10000))
+    List_2.append(random.randint(0,10000))
+Result = []
+for k in List_1:
+    if k in List_1 and k not in List_2:
+      Result.append (k)
+print (Result)

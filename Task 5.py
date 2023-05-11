8b296eff4d3b1f70b44c8dda51eb36e4b23a1d21
NadezhdaZakharova <nadika.zakharova@gmail.com>
2023-05-11 20:54:00 +0300

    upd

---
+++ Task 5.py
@@ -0,0 +1,20 @@
+import random
+#a=(random.uniform(1000,99999))
+#b=(random.uniform(1000,99999))
+a = 1001.25
+b = 2002.37760505
+c=float(a+b)
+
+def eqv(a, b, c):
+    if a<b:
+        eps=0.0001*b
+    else:
+        eps=0.0001*b   
+    if eps > (c - a - b):
+        return True
+    else:
+        return False
+
+print (eqv(a,b,c))
+print (a,b,c)
+

3aad7f9c36935a15093c450759e31486b2ec2394
NadezhdaZakharova <nadika.zakharova@gmail.com>
2025-04-06 18:40:23 +0300

    -

--- Шифры_1/NewTable.py
+++ Шифры_1/NewTable.py
@@ -1,19 +1,21 @@
 def create_table(key, message):
     key = "".join(sorted(set(key), key=lambda k: key.index(k)))
+    key = key.lower()
     num_columns = len(key)
     rows = [message[i:i + num_columns] for i in range(0, len(message), num_columns)]
     if len(rows[-1]) < num_columns:
         rows[-1] += ' ' * (num_columns - len(rows[-1]))
     table = [key] + rows
-    
+
     return table
 
 def sort_table(table):
     header = table[0]
     sorted_indices = sorted(range(len(header)), key=lambda i: header[i])
     sorted_table = []
-    for row in table:
-        sorted_row = ''.join(row[i] for i in sorted_indices)
+    
+    for i in range(len(table)):
+        sorted_row = ''.join(table[i][j] for j in sorted_indices)
         sorted_table.append(sorted_row)
     
     return sorted_table
@@ -29,7 +31,14 @@ def main():
 
     table = create_table(key, message)
     sorted_table = sort_table(table)
-    result = ''.join(sorted_table[1:])
-    print("Результат:", result)
+
+    result = []
+    
+    for col in range(len(sorted_table[0])):
+        for row in range(1, len(sorted_table)): 
+            result.append(sorted_table[row][col])
+    
+    print("Результат:", ''.join(result))
+
 if __name__ == "__main__":
     main()
